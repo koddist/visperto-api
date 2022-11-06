db8000513f40144f5eb0d4298d73ef72c38b433c
Ayaz Guliyev <ayaz@MacBook-Pro.fritz.box>
2022-11-06 12:29:54 +0100

    prettier styling

--- src/dto/visa_country.dto.ts
+++ src/dto/visa_country.dto.ts
@@ -1,11 +1,11 @@
-import { IsArray, IsNotEmpty, IsString } from "class-validator";
-import { VisaRequirementDto } from "./visa_requirement.dto";
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { VisaRequirementDto } from './visa_requirement.dto';
 
 export class VisaCountryDto {
-    @IsString()
-    @IsNotEmpty()
-    readonly name: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
 
-    @IsArray()
-    readonly visa_requirements: VisaRequirementDto[];
+  @IsArray()
+  readonly visa_requirements: VisaRequirementDto[];
 }
--- src/schemas/country.schema.ts
+++ src/schemas/country.schema.ts
@@ -1,25 +1,26 @@
-import { Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 export type CountryDocument = Country & Document;
 
 @Schema({ collection: 'countries' })
 export class Country extends Document {
-
-  @Prop(raw({
-    common: { type: String },
-    official: { type: String },
-    nativeName: {
-      eng: {
-        official: { type: String },
-        common: { type: String }
-      }
-    }
-  }))
+  @Prop(
+    raw({
+      common: { type: String },
+      official: { type: String },
+      nativeName: {
+        eng: {
+          official: { type: String },
+          common: { type: String },
+        },
+      },
+    }),
+  )
   name: string;
 
   @Prop([String])
-  tld: string[]
+  tld: string[];
 
   @Prop()
   cca2: string;
@@ -41,8 +42,6 @@ export class Country extends Document {
 
   @Prop()
   unMember: boolean;
-
-
 }
 
 export const CountrySchema = SchemaFactory.createForClass(Country);
