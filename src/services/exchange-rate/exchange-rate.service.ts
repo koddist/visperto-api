11909a54a6417161a8c0a586f39a3593164d0f6f
Ayaz Guliyev <ayaz@Air-von-Ayaz.fritz.box>
2023-10-04 22:47:02 +0200

    exchange rates API bug is fixed

--- src/services/exchange-rate/exchange-rate.service.ts
+++ src/services/exchange-rate/exchange-rate.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { catchError, map, Observable, throwError } from 'rxjs';
+import { LogtailService } from '../logtail/logtail.service';
 
 @Injectable()
 export class ExchangeRateService {
-  constructor(private readonly httpService: HttpService) {}
+  private readonly apiAccessKey = 'c0e18f321af8a40b013a12468e0a1268';
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly logtailService: LogtailService,
+  ) {}
 
   public getExchangeRate(
     baseCurrency: string,
     quoteCurrency: string,
   ): Observable<any> {
-    const headers = { 'Accept-Encoding': 'gzip,deflate,compress' };
     return this.httpService
-      .get(`https://api.exchangerate.host/latest?base=${baseCurrency}`, {
-        headers,
-      })
+      .get(
+        `http://api.exchangerate.host/live?access_key=${this.apiAccessKey}&source=${baseCurrency}`,
+      )
       .pipe(
         map((res) => {
-          if (res.data && res.data.rates) {
-            return { [quoteCurrency]: res.data.rates[quoteCurrency] };
-          } else {
-            throw new Error('Invalid response from API');
+          if (res.data && res.data.quotes && baseCurrency !== quoteCurrency) {
+            return {
+              [quoteCurrency]:
+                res.data.quotes[`${baseCurrency}${quoteCurrency}`],
+            };
+          } else if (baseCurrency === quoteCurrency) {
+            return { [quoteCurrency]: 1 };
           }
         }),
         catchError((e) => {
+          this.logtailService.logError(
+            'getting exchange rates are failed',
+            'exchangeRates',
+            e,
+          );
           return throwError(e);
         }),
       );
